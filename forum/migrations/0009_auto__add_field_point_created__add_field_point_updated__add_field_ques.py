657bdb6af770c9aa526bfec3154785dfd98f48a1
Joshua Williams <jowillia@gmail.com>
2013-02-02 10:51:21 -0500

    adding created timestamps in database

---
+++ forum/migrations/0009_auto__add_field_point_created__add_field_point_updated__add_field_ques.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Point.created'
+        db.add_column('forum_point', 'created',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 2, 2, 0, 0), auto_now_add=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Point.updated'
+        db.add_column('forum_point', 'updated',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 2, 2, 0, 0), auto_now=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Question.created'
+        db.add_column('forum_question', 'created',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 2, 2, 0, 0), auto_now_add=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Question.updated'
+        db.add_column('forum_question', 'updated',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 2, 2, 0, 0), auto_now=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Answer.created'
+        db.add_column('forum_answer', 'created',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 2, 2, 0, 0), auto_now_add=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Answer.updated'
+        db.add_column('forum_answer', 'updated',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 2, 2, 0, 0), auto_now=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Point.created'
+        db.delete_column('forum_point', 'created')
+
+        # Deleting field 'Point.updated'
+        db.delete_column('forum_point', 'updated')
+
+        # Deleting field 'Question.created'
+        db.delete_column('forum_question', 'created')
+
+        # Deleting field 'Question.updated'
+        db.delete_column('forum_question', 'updated')
+
+        # Deleting field 'Answer.created'
+        db.delete_column('forum_answer', 'created')
+
+        # Deleting field 'Answer.updated'
+        db.delete_column('forum_answer', 'updated')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'forum.answer': {
+            'Meta': {'object_name': 'Answer'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 2, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'points': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'apoint+'", 'symmetrical': 'False', 'to': "orm['forum.Point']"}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 2, 0, 0)', 'auto_now': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'forum.point': {
+            'Meta': {'object_name': 'Point'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 2, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'fromuser': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 2, 0, 0)', 'auto_now': 'True', 'blank': 'True'})
+        },
+        'forum.question': {
+            'Meta': {'object_name': 'Question'},
+            'answers': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'answer+'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['forum.Answer']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 2, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'points': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'qpoint+'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['forum.Point']"}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'max_length': '50', 'populate_from': "'title'", 'unique_with': '()'}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 2, 0, 0)', 'auto_now': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'taggit.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'taggit.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_tagged_items'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_items'", 'to': "orm['taggit.Tag']"})
+        }
+    }
+
+    complete_apps = ['forum']
