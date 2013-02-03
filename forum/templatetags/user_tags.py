a61c5d20e2f147444687bf178d52663b72dc29be
Joshua Williams <jowillia@gmail.com>
2013-02-03 08:52:53 -0500

    proper summing

--- forum/templatetags/user_tags.py
+++ forum/templatetags/user_tags.py
@@ -6,8 +6,8 @@ register = template.Library()
 def user_points(user):
     result = 0
     for question in Question.objects.filter(user__id__exact=user.id):
-        result=+question.points.count()
+        result+=question.points.count()
     for answer in Answer.objects.filter(user__id__exact=user.id):
-        result=+answer.points.count()
+        result+=answer.points.count()
     return result
 
