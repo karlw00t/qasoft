de07eda6b902c2cc7c3bd2cb99b7ff4ad9b4e8f0
Joshua Williams <jowillia@gmail.com>
2013-02-06 20:13:30 -0500

    better form label

--- forum/forms.py
+++ forum/forms.py
@@ -1,12 +1,22 @@
 from django.forms import ModelForm
+from django import forms
 from forum.models import Question, Answer
 
 class QuestionForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        self.fields['text'].label = "Your Question"
+
     class Meta:
         model = Question
         exclude = ('user','points', 'answers')
 
 class AnswerForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AnswerForm, self).__init__(*args, **kwargs)
+        self.fields['text'].label = "Your Answer"
+
     class Meta:
         model = Answer
         exclude = ('user', 'points')
+        text =forms.DateField(label='Publication date')
