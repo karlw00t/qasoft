09f70ad85f1787dc231675d6cc2f49e73642894d
Joshua Williams <jowillia@gmail.com>
2013-02-14 21:31:29 -0500

    still trying

--- qasoft/urls.py
+++ qasoft/urls.py
@@ -3,6 +3,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import ajax
 import forum
 
+from django.conf import settings
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
